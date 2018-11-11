7ad4c0fe88b8b9b5e268b938380c49210ab39b08
Pavillin <dylansprague11@hotmail.com>
2018-11-11 17:20:04 -0500

    init

---
+++ src/main/java/com/deadfyre/fyrebot/Main.java
@@ -0,0 +1,29 @@
+package com.deadfyre.fyrebot;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("Please provide a valid token as the first argument!");
+            return;
+        }
+        // The token is the first argument of the program
+        String token = args[0];
+
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+
+        // Add a listener which answers with "Pong!" if someone writes "!ping"
+        api.addMessageCreateListener(event -> {
+            if (event.getMessage().getContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+            }
+        });
+
+        // Print the invite url of your bot
+        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
+    }
+
+}
